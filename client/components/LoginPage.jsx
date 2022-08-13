688c43c16999885b9b6d3b0bebe9b258dd0e1029
micaylenbrenner <micaylen.brenner@gmail.com>
2022-08-13 13:47:42 -0700

    login page

---
+++ client/components/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react';
+
+class LoginPage extends Component {
+
+  render() {
+    return (
+     <div id="loginPage">
+      <h3>Login to Spotify here...</h3>
+      <button className="login">Spotify Login</button>
+     </div> 
+    )
+  };
+};
+
+
+
+
+export default LoginPage
