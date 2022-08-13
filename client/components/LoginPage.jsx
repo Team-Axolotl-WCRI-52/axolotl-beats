445258f37ec983af22296876c20133723274b33d
micaylenbrenner <micaylen.brenner@gmail.com>
2022-08-13 15:27:08 -0700

    login page fixed

--- client/components/LoginPage.jsx
+++ client/components/LoginPage.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { Component } from 'react';
 
 class LoginPage extends Component {
+  contructor () {
+    super()
+  }; 
+  loginEvent = () => {
+   render()  
+    fetch('/api/auth')
+      .then(response => response.json())
+      .then(res => console.log(res))
+      .catch(err => console.log(err, 'auth fetch failed'))
+    }
+    
+    render() {
+      return (
+       <div id="loginPage">
+        <h3>Login to Spotify here...</h3>
+        <button className="login" onClick={this.loginEvent}>Spotify Login</button>
+       </div> 
+      )
+    };
+}
 
-  render() {
-    return (
-     <div id="loginPage">
-      <h3>Login to Spotify here...</h3>
-      <button className="login">Spotify Login</button>
-     </div> 
-    )
-  };
-};
 
 
 
