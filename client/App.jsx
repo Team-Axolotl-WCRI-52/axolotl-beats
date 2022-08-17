5ac835f73649d6238823aa50018691f3abbf3acb
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-08-17 10:04:31 -0700

    tailwind, mui, prettier configured

--- client/App.jsx
+++ client/App.jsx
@@ -5,62 +5,79 @@ import LoginPage from './components/LoginPage.jsx';
 import PlaylistPage from './components/PlaylistPage.jsx';
 import EmbeddedPlayer from './components/EmbeddedPlayer.jsx';
 import TempReroute from './components/TempReroute.jsx';
+import Slider from '@mui/material/Slider';
 
 import './stylesheets/styles.scss';
 import './stylesheets/app.css';
 
-
 // make App stateful -> class component
-// define a handler that updates state (setState) 
+// define a handler that updates state (setState)
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      playlistId: 'Initial value'
+      playlistId: 'Initial value',
     };
-    // bind handler this component 
+    // bind handler this component
     this.updatePlaylistId = this.updatePlaylistId.bind(this);
   }
 
   // should this be tied in with a lifecycle method?
-  // define handler for playlistFormSubmit that will get passed down to PlaylistForm component 
+  // define handler for playlistFormSubmit that will get passed down to PlaylistForm component
   // this needs to be passed down to PlaylistPage then to PlaylistForm
-  updatePlaylistId (id) {
+  updatePlaylistId(id) {
     console.log('argument id is:', id);
-    // should update state to set playlistId to the one returned from the form 
-    this.setState({playlistId: id})
+    // should update state to set playlistId to the one returned from the form
+    this.setState({ playlistId: id });
     // re-render?
     console.log('this.state.playlistId is: ', this.state.playlistId);
   }
 
-
   render() {
-
     console.log('this.state in render', this.state.playlistId);
 
     return (
       <Router>
         <div id='app'>
           <nav id='navBar'>
-              <ul className="navList">
-                <li className='links' id='logoutButton' ><a className='bye' href='http://google.com'>Logout</a></li>
-              </ul>
-            </nav>
+            <ul className='navList'>
+              <li className='links' id='logoutButton'>
+                <a className='bye' href='http://google.com'>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
           <div id='title'>
             <div id='name'>Axolotl Beats</div>
             <div id='slogan'>beats forEach</div>
           </div>
-        <Routes>
-          <Route path = '/' element={ <LoginPage /> }></Route>
-          <Route path='/player' element={ <EmbeddedPlayer playlistId={this.state.playlistId}/> }></Route>
-          <Route path='/playlistform' element={ <PlaylistPage updatePlaylistId={this.updatePlaylistId}/> }></Route>
-      </Routes>
-      </div>
-      <nav className='end' id='navBar'>End</nav>
-    </Router>
-    )
+          <Routes>
+            <Route path='/' element={<LoginPage />}></Route>
+            <Route
+              path='/player'
+              element={<EmbeddedPlayer playlistId={this.state.playlistId} />}
+            ></Route>
+            <Route
+              path='/playlistform'
+              element={
+                <PlaylistPage updatePlaylistId={this.updatePlaylistId} />
+              }
+            ></Route>
+          </Routes>
+        </div>
+        <Slider
+          aria-label='Always visible'
+          defaultValue={80}
+          step={10}
+          valueLabelDisplay='on'
+        />
+        <nav className='end' id='navBar'>
+          End
+        </nav>
+      </Router>
+    );
+  }
 }
-};
 
-export default App; 
-
+export default App;
