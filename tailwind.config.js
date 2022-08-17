04cc6f27be9809b31ad5590bcc8edd5af0f86bfe
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-08-17 09:51:31 -0700

    mui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./client/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      primary: '#fad3da',
+      secondary: '#ffe5d9ff',
+      spotify: rgb(200, 235, 214),
+    },
     extend: {},
   },
   plugins: [],
